26054050e3f36cec09085f31c5da4988be74199a
AndoniAlonsoTort <andoni.alonso-tort@etu.univ-lehavre.fr>
2023-10-04 01:56:50 +0200

    Init page home

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const hostname = 'localhost';
+const port = 3000;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express',    
+    default_directory: 'http://' + hostname + ':' + port
+   });
 });
 
 module.exports = router;

5e98e441429573caa4f52e6bbf4c666a35186a59
TEKFA Fouad <fouad.tekfa@etu.univ-lehavre.fr>
2023-10-07 03:34:00 +0200

    modification du formulaire d'ajout d'annonce pour récupérer les données soumises et insérer les données sans le traitement d'images

--- routes/announces.js
+++ routes/announces.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const Announce = require('../model/announce');
+
 const hostname = 'localhost';
 const port = 3000;
 
@@ -10,5 +12,35 @@ router.get('/create', function(req, res, next) {
     default_directory: 'http://' + hostname + ':' + port
    });
 });
+
+router.post('/create', async (req, res) => {
+  try {
+    // recuperer les données du formulaire
+    const { name, type, published, status, description, price, date, photos } = req.body;
+      console.log(name, type, published, status, description, price, date, photos);
+    // créer une nouvelle annonce
+    const newAnnounce = new Announce({
+      name,
+      type,
+      published,
+      status,
+      description,
+      price,
+      date,
+      photos,
+    });
+
+    console.log(newAnnounce);
+
+    // enregistrez l'annonce dans la base de données
+    await newAnnounce.save();
+    res.redirect('/'); // rediriger l'utilisateur vers la page d'accueil
+  } catch (error) {
+    console.error(error);
+    //pour indiquer au client qu'une erreur interne du serveur s'est produite lors de la création de l'annonce
+    res.status(500).json({ message: 'une erreur s\'est produite lors de la création de l\'annonce' });
+  }
+});
+
 
 module.exports = router;
